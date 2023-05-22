66481da3bb59eefc737a4811569488b069cf424d
exmmth <b4mmth@gmail.com>
2023-05-22 19:14:50 +0500

    Line gap modifiers in radio, checbox lists

--- elements/rfformradio/templates/template.php
+++ elements/rfformradio/templates/template.php
@@ -22,14 +22,15 @@ if(empty($props['field_name']))
 <?php
 if(isset($props['linebreak']) && $props['linebreak'])
 {
-    $class="uk-grid uk-grid-small";
+    $class  = "uk-grid";
+    $class .= ($props['linebreak_gap'] != '') ? " uk-grid-" . $props['linebreak_gap'] : "";
 }
 else
 {
     $class="uk-form-controls";
 }
 ?>
-    <div class="<?= $class ?>>">
+    <div class="<?= $class ?>">
         <?php foreach ($children as $child) : ?>
             <?= $builder->render($child, ['element' => $props, 'i' => $i]) ?>
             <?php $i++ ?>
